0cab96dc784a1cfe212ea94287f606606ddd8de4
David Torrubia <davidtorrubiasantos@gmail.com>
2023-10-01 23:07:26 +0200

    First component Design

---
+++ src/components/btns/NewProjectBtn.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="rounded flex m-2 duration-100 hover:shadow-xl bg-sky-500/50 hover:bg-sky-500 text-white/50 hover:text-white">
+        <PlusIcon class="h-16 text-inherit"/>
+    </button>
+</template>
+<script>
+import { PlusIcon } from '@heroicons/vue/24/solid';
+export default {
+    name: 'NewProjectBtn',
+    components: {PlusIcon}
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewProjectBtn/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import NewProjectBtn from '@/components/btns/NewProjectBtn.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    NewProjectBtn
   }
 }
 </script>
