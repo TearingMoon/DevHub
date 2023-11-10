f9f4db215474132d1c67abea32a065eb9d3848cf
David Torrubia <davidtorrubiasantos@gmail.com>
2023-11-10 18:20:02 +0100

    Transformed Elements to Typescript and Removed Prettier from linting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store";
+import store from "./store/mainStore.ts";
 import i18n from "./i18n";
-import router from "./router";
+import router from "./router/router.ts";
 import "./assets/tailwind.css";
 
 createApp(App).use(router).use(i18n).use(store).mount("#app");
