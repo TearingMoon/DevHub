37226b3c68f63f4f43720ded247e7bba36be2187
David Torrubia <davidtorrubiasantos@gmail.com>
2023-11-10 11:40:59 +0100

    Implemented vue-router

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import i18n from "./i18n";
+import router from "./router";
 
-createApp(App).use(i18n).use(store).mount("#app");
+createApp(App).use(router).use(i18n).use(store).mount("#app");
